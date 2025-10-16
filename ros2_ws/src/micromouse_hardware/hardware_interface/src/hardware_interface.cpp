694c8e5f44d9057ca438517fab64843645c95f03
Xiation <abyanraditya.rakapasha@gmail.com>
2025-10-16 18:35:58 +0700

    modfied hardware_interface

--- ros2_ws/src/micromouse_hardware/hardware_interface/src/hardware_interface.cpp
+++ ros2_ws/src/micromouse_hardware/hardware_interface/src/hardware_interface.cpp
@@ -365,7 +365,7 @@ namespace ratze_hardware_interface
         q.setRPY(roll, pitch, yaw);
         q.normalize();
 
-        RCLCPP_ERROR(this->get_logger(), "YAWWWWW: %s", yaw.c_str());
+        RCLCPP_ERROR(this->get_logger(), "YAWWWWW: %f", yaw);
 
 
         imu_msg.orientation.x = q.x();
